83611e8e76cb63345c06aeca62d7af59c076b404
SejalBhardwaj20 <BHARDWAJSEJAL20@GMAIL.COM>
2026-03-28 01:25:06 +0530

    commit
    commit

--- src/components/LoadingPage.jsx
+++ src/components/LoadingPage.jsx
@@ -1,19 +1,20 @@
 import { useEffect, useState } from 'react'
 import { SpiralAnimation } from './ui/spiral-animation'
+import LightRays from './ui/LightRays'
 
 export default function LoadingPage({ onComplete }) {
   const [textVisible, setTextVisible] = useState(false)
 
   useEffect(() => {
-    // Fade in the loading text immediately
+    // Fade in the loading text after animation loads
     const textTimer = setTimeout(() => {
       setTextVisible(true)
-    }, 300)
+    }, 1000)
 
-    // Complete loading much faster - just 1.5 seconds
+    // Complete loading after animation plays
     const completeTimer = setTimeout(() => {
       onComplete()
-    }, 1500)
+    }, 4000)
 
     return () => {
       clearTimeout(textTimer)
@@ -23,6 +24,25 @@ export default function LoadingPage({ onComplete }) {
 
   return (
     <div className="fixed inset-0 w-full h-full overflow-hidden bg-black">
+      {/* Light Rays Background */}
+      <div className="absolute inset-0 z-0">
+        <LightRays
+          raysOrigin="top-center"
+          raysColor="#ffffff"
+          raysSpeed={1}
+          lightSpread={0.5}
+          rayLength={3}
+          followMouse={true}
+          mouseInfluence={0.1}
+          noiseAmount={0}
+          distortion={0}
+          className="custom-rays"
+          pulsating={false}
+          fadeDistance={1}
+          saturation={1}
+        />
+      </div>
+
       {/* Spiral Animation */}
       <div className="absolute inset-0 z-1">
         <SpiralAnimation />
@@ -31,7 +51,7 @@ export default function LoadingPage({ onComplete }) {
       {/* Loading Text with Pulsing Effect */}
       <div 
         className={`absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-10
-          transition-all duration-700 ease-out
+          transition-all duration-1500 ease-out
           ${textVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-4'}`}
       >
         <div className="text-white text-6xl tracking-[0.3em] uppercase font-extralight animate-pulse">
